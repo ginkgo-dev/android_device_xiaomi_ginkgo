a73fe8886c99aa5636d0974729131e5242f063cd
DarkJoker360 <simoespo159@gmail.com>
2021-10-18 10:09:41 +0200

    ginkgo: Switch back to stock build desc
    * Fixes device recognition.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -26,10 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210905.001 7511028 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
