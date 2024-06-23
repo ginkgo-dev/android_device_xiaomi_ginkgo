7c385413943ca80560e14288901cb9c6ebd9805e
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    ginkgo: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Vendor blobs
 $(call inherit-product, vendor/xiaomi/ginkgo/ginkgo-vendor.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
